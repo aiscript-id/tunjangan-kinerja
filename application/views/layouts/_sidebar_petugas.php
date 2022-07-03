31a40df37bf9275284e127a3a49864d4a4c9bf3e
unknown <ibnu.setia23@gmail.com>
2022-07-03 19:54:34 +0800

    main

--- application/views/layouts/_sidebar_petugas.php
+++ application/views/layouts/_sidebar_petugas.php
@@ -4,7 +4,7 @@
     <!-- Brand Logo -->
     <a href="../../index3.html" class="brand-link">
       <img src="<?= base_url("$logo") ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light" style="font-size: 9pt">TUKIN BPPNDDTT BANJARMASIN</span>
+      <span class="brand-text font-weight-light" style="font-size: 9pt">TUKIN BPPMDDTT BANJARMASIN</span>
     </a>
     <div class="sidebar">
         <!-- Sidebar user (optional) -->
